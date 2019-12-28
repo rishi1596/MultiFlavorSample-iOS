57cb77aebb138a22c661413ddb62ca753a0a1e14
Rishabh <rishabh@dronamobile.com>
2019-12-28 19:21:04 +0530

    Added 2nd flavor

---
+++ MultiFlavorSample/MaxTech/AppConstants.h
@@ -0,0 +1,18 @@
+//
+//  AppConstants.h
+//  MaxTech
+//
+//  Created by Rishabh Jain on 28/12/19.
+//  Copyright © 2019 Rishabh Jain. All rights reserved.
+//
+
+#ifndef AppConstants_h
+#define AppConstants_h
+
+
+#define COMPANY_NAME @"MAX Company"
+#define COMPANY_DESCRIPTION @"DUMMY DATA IT"
+
+#define APP_BACKGROUND_COLOR [UIColor blueColor]
+
+#endif /* AppConstants_h */
