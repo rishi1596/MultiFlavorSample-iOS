df3b0c662668407141a296a12f1e988cd54d7038
Rishabh <rishabh@dronamobile.com>
2019-12-28 19:26:44 +0530

    added 3rd flavor

---
+++ MultiFlavorSample/CrypticTech/AppConstants.h
@@ -0,0 +1,17 @@
+//
+//  AppConstants.h
+//  CrypticTech
+//
+//  Created by Rishabh Jain on 28/12/19.
+//  Copyright © 2019 Rishabh Jain. All rights reserved.
+//
+
+#ifndef AppConstants_h
+#define AppConstants_h
+
+#define COMPANY_NAME @"Cryptic Company"
+#define COMPANY_DESCRIPTION @"DUMMY DATA IT"
+
+#define APP_BACKGROUND_COLOR [UIColor blueColor]
+
+#endif /* AppConstants_h */
